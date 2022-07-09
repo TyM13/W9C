12e0c46e857ca1479c557764c5481d49e2abe54a
Your Name <you@example.com>
2022-07-08 18:32:26 -0600

    added various js variables, used push/pop abd selector variables

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let item_names = [`name1`, `name2`, `name3`];
+let item_prices = [10, 20, 30];
+let in_stock = [`true`, `false`, `true`];
+
+let first_item_name = item_names[0];
+let first_item_price = item_prices [0];
+
+in_stock.push(`true`);
+item_names.push(`name2`);
+item_prices.push(30);
+
+let last_item_name = item_names.pop ();
+
+let last_item_price = item_prices.pop ();
+
+let last_item_stock = in_stock.pop ();
